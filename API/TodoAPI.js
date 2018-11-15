0c63411770835a59e37df69e0633275c0f7ae983
willshepp28 <willsheppard28@outlook.com>
2018-11-15 16:17:04 -0500

    created a endpoint where user can get complete and incomplete todos

--- API/TodoAPI.js
+++ API/TodoAPI.js
@@ -24,6 +24,40 @@ router.get("/", async(request, response) => {
 
 
 
+// Get all todos that are completed
+router.get("/complete/:userId", async(request, response) => {
+
+    try {
+        const todos = await models.Todo.findAll({
+            where: { userId: parseInt(request.params.userId), complete: true}
+        })
+        return response.status(200).json(todos);
+    }
+    catch(error) {
+        console.log(error);
+        return response.status(400).send("error");
+    }
+});
+
+
+
+
+// Get all todos that are incomplete
+router.get("/incomplete/:userId", async(request, response) => {
+
+    try {
+        const todos = await models.Todo.findAll({
+            where: { userId: parseInt(request.params.userId), complete: false}
+        })
+        return response.status(200).json(todos);
+    }
+    catch(error) {
+        console.log(error);
+        return response.status(400).send("error");
+    }
+});
+
+
 // Get all todos by userId
 router.get("/:id", async(request, response) => {
 
@@ -42,7 +76,7 @@ router.get("/:id", async(request, response) => {
 
 
 // Mark todo as completed
-router.get("/completed/:todoId", async(request, response) => {
+router.get("/mark-complete/:todoId", async(request, response) => {
 
     try {
         const todos = await models.Todo.update({ complete: true},
@@ -56,6 +90,9 @@ router.get("/completed/:todoId", async(request, response) => {
         return response.status(400).send(error);
     }
 });
+
+
+
 
 
 module.exports = router;
