266f7b3910f6f0947ef71252f72f8252086ef617
willshepp28 <willsheppard28@outlook.com>
2018-11-18 19:13:34 -0500

    installed jsonwebtoken and completed signpu route

--- API/API.js
+++ API/API.js
@@ -2,11 +2,21 @@ const router = require("express").Router();
 const models = require("../db/models");
 
 
-router.get("/", (request, response) => {
+router.get("/", async(request, response) => {
     return response.status(200).json({ message: "You have entered my API"});
 });
 
+// router.get("/users", async(request, response) => {
+//     try {
+//         const users = await models.User.findAll();
 
+//         return response.status(200).json(users);
+//     }
+//     catch(error){
+//         console.log(error);
+//         return response.status(400).send(error);
+//     }
+// })
 
 
 
--- API/AuthenticationAPI.js
+++ API/AuthenticationAPI.js
@@ -1,9 +1,54 @@
 const router = require("express").Router();
+const models = require("../db/models");
+const jwt = require("jsonwebtoken");
 
 
-// This is the signup endpoint
+
+
+/*
+|--------------------------------------------------------------------------
+|  Signup Api - the endpoint where users signup
+|--------------------------------------------------------------------------
+*/
 router.post("/signup", async(request, response) => {
-    return response.status(200).send("You are in the signup endpoint");
+
+    console.log(request.body);
+
+    console.log("In the signup endpoint");
+    try{
+
+        if(request.body.username && request.body.password) {
+            const signup = await models.User.create(request.body);
+
+            return response.status(200).send("Successfully signed up new user");
+        }
+    }
+    catch(error){
+        return response.status(400).send("Error");
+    }
+   
+});
+
+
+
+
+
+router.post("/login", async(request, response) => {
+
+    console.log(request.body);
+
+    console.log("In the login endpoint");
+
+    try {
+        const login = await models.User.create(request.body);
+
+        return response.status(200).send();
+    }
+    catch(error){
+        console.log(error);
+        return response.status(400).send("Error");
+    }
+
 });
 
 
